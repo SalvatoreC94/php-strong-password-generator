cf10924da5be8c36d78952570d19fd4001869496
SalvatoreC94 <salvatorecozzolino.1594@gmail.com>
2024-10-01 10:30:25 +0200

    Milestone 2

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+include __DIR__ 'functions.php';
+
+// Funzione per generare una password casuale
+function generatePassword($length) {
+    $lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    $uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $numbers = '0123456789';
+    $symbols = '!@#$%^&*()-_=+[]{};:,.<>?';
+    
+    // Combina tutti i caratteri disponibili
+    $allCharacters = $lowercase . $uppercase . $numbers . $symbols;
+    
+    // Genera la password
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = rand(0, strlen($allCharacters) - 1);
+        $password .= $allCharacters[$randomIndex];
+    }
+    
+    return $password;
+}
+?>
--- index.php
+++ index.php
@@ -1,23 +1,4 @@
-<?php
-// Funzione per generare una password casuale
-function generatePassword($length) {
-    $lowercase = 'abcdefghijklmnopqrstuvwxyz';
-    $uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $numbers = '0123456789';
-    $symbols = '!@#$%^&*()-_=+[]{};:,.<>?';
-    
-    // Combina tutti i caratteri disponibili
-    $allCharacters = $lowercase . $uppercase . $numbers . $symbols;
-    
-    // Genera la password
-    $password = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomIndex = rand(0, strlen($allCharacters) - 1);
-        $password .= $allCharacters[$randomIndex];
-    }
-    
-    return $password;
-}
+<?
 
 // Controlla se è stata inviata una lunghezza della password
 if (isset($_GET['length']) && is_numeric($_GET['length'])) {
@@ -52,6 +33,6 @@ if (isset($_GET['length']) && is_numeric($_GET['length'])) {
         </div>
     </div>
 
- 
+   
 </body>
 </html>
